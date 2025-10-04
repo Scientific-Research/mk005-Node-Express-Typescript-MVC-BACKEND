45154d35bacb804b07f83fe5732fe985d0da40d0
Maximilian <scientificresearch309@gmail.com>
2025-10-04 22:55:53 +0200

    getting the list of the jobs when we have only slash in the URL

--- src/model.ts
+++ src/model.ts
@@ -1,3 +1,5 @@
 export const getApiDocumentationHtml = () => {
-  return 'job site Api2222';
+  return `<h1>GET A JOB API</h1> <ul>
+  <li><a href="/jobs">/jobs</a>- returns an array of job objects</li>
+  </ul>`;
 };
